c708f3e749628314023e7b40dc810519f9c908b2
dishantsethi <dishantsethi14@gmail.com>
2021-05-02 20:33:58 +0530

    add dropup

--- src/components/MenuSidebar.js
+++ src/components/MenuSidebar.js
@@ -17,8 +17,25 @@ function MenuSidebar(props) {
         <div id="taskbar-wrapper">
             <div id="taskbar">
                 <div id="quickstart">
-                    <span class="iconify" data-icon="ant-design:windows-filled" data-inline="false" style={{boxShadow: '100px'}}></span>
-                    <span class="iconify" data-icon="fluent:mail-28-filled" data-inline="false"></span>
+                    <div class="dropup">
+                      <div class="dropup-content">
+                        <a>About</a>
+                        <a>Skills</a>
+                        <a>Experience</a>
+                        <a>Projects</a>
+                        <a>Education</a>
+                        <a>About You</a>
+                        <a>Hire Me</a>
+                        <a>Donate</a>
+                      </div>
+                      <span class="iconify" data-icon="ant-design:windows-filled" data-inline="false" style={{boxShadow: '100px'}}></span>
+                    </div>
+                    <div class="dropup">
+                      <div class="dropup-content">
+                        <a>Ah! No New message. You're Single!</a>
+                      </div>
+                      <span class="iconify" data-icon="fluent:mail-28-filled" data-inline="false"></span>
+                    </div>
                     <a href="https://www.google.com" title="Open New Tab" target="blank"><span class="iconify" data-icon="grommet-icons:chrome" data-inline="false"></span></a>
                     <a href="https://www.netflix.com" target="blank"><span class="iconify" data-icon="logos:netflix-icon" data-inline="false"></span></a>
                     <a href={props.data} target="blank"><span class="iconify" data-icon="logos:linkedin-icon" data-inline="false"></span></a>
